ab1943782ba3d274ce311d96acfb8499bfd84b8c
alexg3110 <alexg3110@gmail.com>
2022-07-26 14:38:35 +0200

    WIP register

--- MagicCo-frontend/src/app/register/register.component.ts
+++ MagicCo-frontend/src/app/register/register.component.ts
@@ -1,7 +1,7 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component, Inject, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService, Observable, OValueChangeEvent } from 'ontimize-web-ngx';
 import { RegisterService } from './register.service';
 import { User } from './user';
@@ -16,49 +16,94 @@ import { User } from './user';
 
 export class RegisterComponent implements OnInit {
 
-  registerForm: FormGroup;
+  baseUrl: string = "http://localhost:33333/users/user/";
 
-  constructor() { }
+  registerForm: FormGroup = new FormGroup({});
+  emailPattern: any = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  match: boolean = false;
+
+
+  constructor(private http: HttpClient, private router: Router, @Inject(AuthService) private authService: AuthService) {
+  }
 
   ngOnInit() {
-
+    this.registerForm = this.createForm();
   }
 
-  confirmPassword(value){
-    alert(value);
-  }
 
-  onValueChange(event: OValueChangeEvent) {
-    var password = document.getElementById("password").innerHTML;
-    console.log(document.getElementById("password").innerHTML);
-    //var cpassword = (document.getElementById("cpassword") as HTMLInputElement).value;
-    alert(password);
-    //alert(cpassword);
-    if((<HTMLInputElement>document.getElementById("password")).value != (<HTMLInputElement>document.getElementById("cpassword")).value){
-
-      document.getElementById("errormessage").style.visibility="visible";
-      document.getElementById("buttonregister").disabled="true";
-
-    }else{
-      document.getElementById("buttonregister").disabled="false";
-    }
-
-    /*if (!this.printOnValueChange.checked) {
-      return;
-    }
-    const eventChange = new ConsoleLogEvent();
-    eventChange.eventName = 'onValueChange';
-    eventChange.newValue = event.newValue;
-    eventChange.oldValue = event.oldValue;
-    eventChange.type = event.isProgrammaticChange() ? 'PROGRAMMATIC' : 'USER';
-    eventChange.attr = event.target.oattr;
-
-    this.consoleLog.unshift(eventChange);*/
-    //alert(event.newValue);
-  }
 
   register(){
-    alert("Entra");
+    console.log(this.registerForm.value);
+    /*const data = {
+      "USER_": this.registerForm.value.username,
+      "EMAIL": this.registerForm.value.email,
+      "PASSWORD": this.registerForm.value.password
+    };
+    const sqlTypes = {
+      "USER_":12,
+      "EMAIL":12,
+      "PASSWORD":12
+    };
+    const header = {
+      'content-type': 'application/json',
+      'Authorization': 'Basic ' + btoa('demo:demouser')
+    };
+    console.log(header);
+
+    const body =
+      '{"data": {' + data +'}  ,"sqltypes": {' + sqlTypes+ '}} ';*/
+      var idFinal="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjcmVhdGlvbi10aW1lIjoxNjU3MDA2NjA3MjA3LCJ1c2VybmFtZSI6ImFkbWluaXN0cmF0b3IifQ.ceXatWm60xn4w87yHT-6mZW_Wz3gsTua23pxfS2cTwU";
+    console.log(idFinal);
+            const options ={
+            headers : new HttpHeaders({
+              'Content-Type': 'application/json',
+              'Authorization': "Bearer "+ idFinal
+            }),
+            body: {
+              "data" : {
+                "USER_": this.registerForm.value.username,
+                "EMAIL": this.registerForm.value.email,
+                "PASSWORD": this.registerForm.value.password
+              },
+              "sqlTypes" : {
+                "USER_":12,
+                "EMAIL":12,
+                "PASSWORD":12
+              }
+            }
+          }
+          console.log(options);
+      //this.http.post(this.baseUrl, body, { headers: header })
+      this.http.post(this.baseUrl, options).subscribe(
+        data => { // json data
+          console.log('Success: ', data);
+          this.router.navigate(['../login']);
+      },
+      error => {
+          console.log('Error: ', error);
+      }
+      );
+  }
+
+
+
+ createForm() {
+    //console.log('method : createForm')
+    return new FormGroup({
+      username: new FormControl('', [Validators.minLength(3), Validators.maxLength(25)]),
+      email: new FormControl('', [Validators.minLength(3), Validators.pattern(this.emailPattern)]),
+      password: new FormControl('', [Validators.minLength(6), Validators.maxLength(25)]),
+      confirmPass: new FormControl()
+    });
+  }
+  checkPasswords(){
+    let stringpass = this.registerForm.value.password;
+    let stringrepeat = this.registerForm.value.confirmPass;
+    if(stringpass != stringrepeat){
+      //alert(this.registerForm.valid);
+      this.match = true;
+    }
+
   }
 
 }
