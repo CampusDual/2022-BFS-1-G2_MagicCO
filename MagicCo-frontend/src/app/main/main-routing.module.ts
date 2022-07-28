0dbf8821b08f59975861ced3966b17182c0a6494
blcpaulnew <grandline.supervisor@gmail.com>
2022-07-28 12:47:24 +0200

    wip user fix

--- MagicCo-frontend/src/app/main/main-routing.module.ts
+++ MagicCo-frontend/src/app/main/main-routing.module.ts
@@ -12,8 +12,7 @@ export const routes: Routes = [
     children: [
       { path: '', redirectTo: 'home', pathMatch: 'full' },
       { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
-      { path: 'services', loadChildren: () => import('./services/services.module').then(m => m.ServicesModule) },
-      { path: 'settings', loadChildren: () => import('./settings/settings.module').then(m => m.SettingsModule) }
+      { path: 'services', loadChildren: () => import('./services/services.module').then(m => m.ServicesModule) }
     ]
   }
 ];
