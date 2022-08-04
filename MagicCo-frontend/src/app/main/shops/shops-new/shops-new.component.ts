43b764375136cff123c5f7953b49747598a2a66e
Rasekov <Rasekov@users.noreply.github.com>
2022-08-04 14:24:58 +0200

    WIP
    2 bugs left

--- MagicCo-frontend/src/app/main/shops/shops-new/shops-new.component.ts
+++ MagicCo-frontend/src/app/main/shops/shops-new/shops-new.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject, OnInit, ViewChild } from '@angular/core';
-import { AuthService, DialogService, OFormComponent, OntimizeService, OTableComponent, OTextInputComponent } from 'ontimize-web-ngx';
+import { AuthService, DialogService, OFormComponent, OFormDataComponent, OntimizeService, OTableComponent, OTextInputComponent } from 'ontimize-web-ngx';
 import { identifierModuleUrl } from '@angular/compiler';
 import { Route, Router } from '@angular/router';
 import { HttpClientModule, HttpHeaders, HttpClient } from '@angular/common/http';
@@ -12,7 +12,7 @@ import { HttpClientModule, HttpHeaders, HttpClient } from '@angular/common/http'
 export class ShopsNewComponent implements OnInit {
 
   @ViewChild('textInput', { read: OTextInputComponent, static: true }) public textInput: OTextInputComponent;
-  @ViewChild('textInput', { read: OFormComponent, static: true }) public form: OFormComponent;
+  @ViewChild('formform', { read: OFormComponent, static: true }) public form: OFormComponent;
 
   constructor(private http: HttpClient,
     private router: Router,
@@ -23,44 +23,9 @@ export class ShopsNewComponent implements OnInit {
   ngOnInit() {
   }
 
-  ngAfterViewInit() {
-    this.form.setFieldValue("user_id", "user");
-  }
-
-  testing(){
-    this.form.setFieldValue("user_id", "user");
-    this.form.insert();
-    this.service.configureService(this.service.getDefaultServiceConfiguration("shops"));
-    const currentDate = new Date().toISOString();
-    const FILTER = {
-      "id_shop": this.form.getFieldValue("id_shop"),
-      "name": this.form.getFieldValue("name"),
-      "region": this.form.getFieldValue("region"),
-      "longitude": this.form.getFieldValue("longitude"),
-      "latitude": this.form.getFieldValue("latitude"),
-      "description": this.form.getFieldValue("description"),
-      "user_id": this.authService.getSessionInfo.name,
-      "start_date": currentDate
-    };
-    console.log(FILTER);
-    alert("Mira la consola");
-  }
-
   onSave() {
-    this.form.setFieldValue("user_id", "user")
+    this.form.setFieldValue("user_id",this.authService.getSessionInfo().user);
     this.form.insert();
-    this.service.configureService(this.service.getDefaultServiceConfiguration("shops"));
-    const currentDate = new Date().toISOString();
-    const FILTER = {
-      "id_shop": this.form.getFieldValue("id_shop"),
-      "name": this.form.getFieldValue("name"),
-      "region": this.form.getFieldValue("region"),
-      "longitude": this.form.getFieldValue("longitude"),
-      "latitude": this.form.getFieldValue("latitude"),
-      "description": this.form.getFieldValue("description"),
-      "user_id": this.authService.getSessionInfo.name,
-      "start_date": currentDate
-    };
-    console.log(FILTER);
+    window.location.reload();
   }
 }
